44212df3405f490dbd379cce158ae25238a9ce90
splittlogic <splittlogic@gmail.com>
2021-10-31 08:45:48 -0500

    first commit

---
+++ src/Facades/bs5extras.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace splittlogic\bs5extras\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class bs5extras extends Facade
+{
+
+
+	protected static function getFacadeAccessor()
+	{
+		return 'bs5extras';
+	}
+
+
+}
---
+++ src/bs5extras.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace splittlogic\bs5extras;
+
+use splittlogic\bs5\bs5;
+
+class bs5extras extends bs5
+{
+
+
+  // Run class as static
+  public static function make()
+  {
+
+    $x = new bs5extras;
+    return $x;
+
+  }
+
+
+}
